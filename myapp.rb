863bbda61584ace08ca14ffa0a2b24b92d460fb3
Sikandar Shukla <bhaity@gmail.com>
2013-10-31 15:28:51 -0400

    add cc email, include error handling for invalid email address, tweak email template

--- myapp.rb
+++ myapp.rb
@@ -4,16 +4,15 @@ require 'nokogiri'
 require 'pony'
 
 Pony.options = {
-  :to => 'bhaity@gmail.com',
-  :from => 'village.green.west.mailer@gmail.com',
+  :from => 'info@villagegreenwest.com',
   :headers => { 'Content-Type' => 'text/html' },
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-    :user_name            => 'village.green.west.mailer@gmail.com',
-    :password             => '245w14st',
+    :user_name            => 'info@villagegreenwest.com',
+    :password             => '245west245',
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
   }
@@ -35,6 +34,8 @@ get '/home' do
 end
 
 post '/mail_to' do
-  Pony.mail :subject => "Village Green West | New Enquiry Received",
-            :body    => erb(:email)
+  Pony.mail :subject => "Village Green West | New Inquiry Received",
+            :body    => erb(:email),
+            :to => params["Email Address"],
+            :cc => 'bhaity@gmail.com'
 end
